4f2b0e0d7554562c5f3c8494304c0fba32110630
Ian <yyh_lee@fitpet.co.kr>
2022-11-20 13:35:46 +0900

    📦️ package.json 추가 및 초기 스크립트 추가

---
+++ src/sync-from-main.ts
@@ -0,0 +1,19 @@
+import { GithubScriptInput } from './types/github-script.type';
+
+function pushBranch(githubScript: GithubScriptInput) {
+    const { repo, owner } = githubScript.context.repo;
+    const prList = githubScript.github.rest.pulls.list({
+        owner,
+        repo,
+    });
+    console.log('--------------- 🖨 Get PR List ---------------');
+    console.log('🔔 PR List: ', prList);
+
+}
+
+export const syncFromMain = async (githubScript: GithubScriptInput) => {
+    console.log('-- Print Github Script Payload --');
+    console.log(githubScript.context.payload);
+
+    await pushBranch(githubScript)
+}
